01ab89a0f622ff3402fa032add1b1d79885fe0ec
phette23 <phette23@gmail.com>
2023-10-26 13:34:32 -0700

    INDUS doesn't want Juniors preloaded, also formatting
    big autoformatting commit which is trivial

--- report_to_enroll.py
+++ report_to_enroll.py
@@ -7,12 +7,12 @@ from openpyxl import load_workbook
 
 
 program_to_course_map = {
-    'Architecture': 'ARCHT-INTRN',
-    'Graduate Architecture': 'MARCH-INTRN',
-    'Graphic Design': 'GRAPH-INTRN',
-    'Industrial Design': 'INDUS-INTRN',
-    'Interaction Design': 'IXDSN-INTRN',
-    'Interior Design': 'INTER-INTRN',
+    "Architecture": "ARCHT-INTRN",
+    "Graduate Architecture": "MARCH-INTRN",
+    "Graphic Design": "GRAPH-INTRN",
+    "Industrial Design": "INDUS-INTRN",
+    "Interaction Design": "IXDSN-INTRN",
+    "Interior Design": "INTER-INTRN",
 }
 programs_with_internship = list(program_to_course_map.keys())
 
@@ -22,7 +22,7 @@ def row_to_dict(header, row):
 
 
 def meets_program_criteria(student):
-    """ Check if student meets the criteria to be added to their major's
+    """Check if student meets the criteria to be added to their major's
     internship course. Criteria rely on the Latest Class Standing field, which
     is a string that goes from "First Year" up to "Fifth Year". It is not a
     _level_ per se, as in an undergrad can be a "Fifth Year" and grad students
@@ -34,18 +34,27 @@ def meets_program_criteria(student):
     Returns:
         bool: true if ready, false otherwise
     """
-    major = student['Primary Program of Study']
+    major = student["Primary Program of Study"]
     # @TODO meet with programs and confirm that this is what they want
-    if major in ('Architecture', 'Interior Design', 'Graphic Design', 'Interaction Design', 'Industrial Design') and student['Latest Class Standing'] not in ('First Year', 'Second Year'):
+    # INDUS wants students to finish Prof Practice, we do not repload them
+    if major in (
+        "Architecture",
+        "Interior Design",
+        "Graphic Design",
+        "Interaction Design",
+    ) and student["Latest Class Standing"] not in ("First Year", "Second Year"):
         return True
-    if major == 'Graduate Architecture' and student['Latest Class Standing'] != 'First Year':
+    if (
+        major == "Graduate Architecture"
+        and student["Latest Class Standing"] != "First Year"
+    ):
         return True
     # default to false
     return False
 
 
 def make_enrollment(student, program=None):
-    """ create an enrollment row if student meets general criteria
+    """create an enrollment row if student meets general criteria
     if program is present, only returns rows for students in that program
 
     Args:
@@ -57,16 +66,20 @@ def make_enrollment(student, program=None):
         if the student is ready, otherwise the boolean False
     """
     # students must be actively enrolled in a program with a required internship
-    major = student['Primary Program of Study']
-    if major in programs_with_internship and student['Primary Program of Study Record Status'] == 'In Progress' and student['CCA Email']:
+    major = student["Primary Program of Study"]
+    if (
+        major in programs_with_internship
+        and student["Primary Program of Study Record Status"] == "In Progress"
+        and student["CCA Email"]
+    ):
         if program and major != program:
             return False
         if not meets_program_criteria(student):
             return False
         # return enrollment row
-        username = re.sub('@cca.edu', '', student['CCA Email'])
+        username = re.sub("@cca.edu", "", student["CCA Email"])
         course = program_to_course_map[major]
-        group = 'International' if student['Is International Student'] == 'Yes' else ''
+        group = "International" if student["Is International Student"] == "Yes" else ""
         return [username, course, group]
     return False
 
@@ -79,19 +92,35 @@ def wd_report_to_enroll_csv(report, program):
     sheet = wb.active
     rows = sheet.iter_rows(values_only=True)
     header = next(rows)
-    with open('enrollments.csv', 'w') as file:
+    with open("enrollments.csv", "w") as file:
         writer = csv.writer(file)
         # write CSV header row
-        writer.writerow(['username', 'course1', 'group1'])
+        writer.writerow(["username", "course1", "group1"])
         for row in rows:
             student = row_to_dict(header, row)
             e = make_enrollment(student, program)
-            if e: writer.writerow(e)
+            if e:
+                writer.writerow(e)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='report_to_enrol', description='Convert Workday internships report into Moodle enrollment CSV.')
-    parser.add_argument('-p', '--program', required=False, choices=programs_with_internship, help='Generate enrollments for only a specific program')
-    parser.add_argument('-r', '--report', required=False, default='Students_for_Internship_Review.xlsx', help='path to the Workday Excel file')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="report_to_enrol",
+        description="Convert Workday internships report into Moodle enrollment CSV.",
+    )
+    parser.add_argument(
+        "-p",
+        "--program",
+        required=False,
+        choices=programs_with_internship,
+        help="Generate enrollments for only a specific program",
+    )
+    parser.add_argument(
+        "-r",
+        "--report",
+        required=False,
+        default="Students_for_Internship_Review.xlsx",
+        help="path to the Workday Excel file",
+    )
     args = parser.parse_args()
     wd_report_to_enroll_csv(args.report, args.program)
--- test_script.py
+++ test_script.py
@@ -3,39 +3,138 @@ import pytest
 from .report_to_enroll import meets_program_criteria, make_enrollment
 
 
-@pytest.mark.parametrize('input,expected', [
-    # not an internship program
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Fake Program', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, False),
-    # not 'in progress'
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'Suspended', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, False),
-    # no email
-    ({'CCA Email': None, 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, False),
-    # domestic enrollment
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, ['a','ARCHT-INTRN','']),
-    # international enrollment
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Interior Design', 'Is International Student': 'Yes', 'Latest Class Standing': 'Third Year'}, ['a','INTER-INTRN','International']),
-])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        # not an internship program
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Fake Program",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            False,
+        ),
+        # not 'in progress'
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "Suspended",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            False,
+        ),
+        # no email
+        (
+            {
+                "CCA Email": None,
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            False,
+        ),
+        # domestic enrollment
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            ["a", "ARCHT-INTRN", ""],
+        ),
+        # international enrollment
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Interior Design",
+                "Is International Student": "Yes",
+                "Latest Class Standing": "Third Year",
+            },
+            ["a", "INTER-INTRN", "International"],
+        ),
+    ],
+)
 def test_make_enrollment(input, expected):
     assert make_enrollment(input) == expected
 
 
-@pytest.mark.parametrize('input,expected', [
-    # not in program
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, False),
-    # in program
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Interior Design', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, ['a','INTER-INTRN','']),
-])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        # not in program
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            False,
+        ),
+        # in program
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Interior Design",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            ["a", "INTER-INTRN", ""],
+        ),
+    ],
+)
 def test_make_enrollment_program_filter(input, expected):
-    assert make_enrollment(input, 'Interior Design') == expected
+    assert make_enrollment(input, "Interior Design") == expected
 
 
-@pytest.mark.parametrize('input,expected', [
-    # ARCHT first year, does not meet
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'First Year'}, False),
-    # ARCHT third year, meets
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Third Year'}, True),
-    # ARCHT > 3rd year, also meets
-    ({'CCA Email': 'a@cca.edu', 'Primary Program of Study Record Status': 'In Progress', 'Primary Program of Study': 'Architecture', 'Is International Student': '', 'Latest Class Standing': 'Fourth Year'}, True),
-])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        # ARCHT first year, does not meet
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "First Year",
+            },
+            False,
+        ),
+        # ARCHT third year, meets
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Third Year",
+            },
+            True,
+        ),
+        # ARCHT > 3rd year, also meets
+        (
+            {
+                "CCA Email": "a@cca.edu",
+                "Primary Program of Study Record Status": "In Progress",
+                "Primary Program of Study": "Architecture",
+                "Is International Student": "",
+                "Latest Class Standing": "Fourth Year",
+            },
+            True,
+        ),
+    ],
+)
 def test_meets_program_criteria(input, expected):
     assert meets_program_criteria(input) == expected
